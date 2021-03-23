ed35875b1e8427c966db72230d78ff93d79e2172
vinayakmaskar <79522753+vinayakmaskar@users.noreply.github.com>
2021-03-23 10:02:52 +0530

    Add files via upload

---
+++ addfirst10naturalnumbers.py
@@ -0,0 +1,11 @@
+a = input("Enter the first number")
+b = input("Enter the second number")
+c = input("Enter the third number")
+
+if a>b & a>c:
+    print(a," is greater")
+else:
+    if b>c:
+        print(b," is greater")
+    else:
+        print(c," is greater")
