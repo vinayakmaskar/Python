f9c7774eaeeb764225248cde2e7e1a2bbc33c154
vinayakmaskar <79522753+vinayakmaskar@users.noreply.github.com>
2021-03-23 10:03:40 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,9 @@
+num = int(input("Enter the number till which addtion has to be performed"))
+
+i = 1
+sum = 0
+
+while i<=num:
+    sum = sum + i
+    i += 1
+print(sum," is the addition of all numbers")
