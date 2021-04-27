29faf8e4893c212ac31c84a105cca010e20ee394
vinayakmaskar <79522753+vinayakmaskar@users.noreply.github.com>
2021-04-27 19:50:04 +0530

    Update ALARM.py

--- ALARM.py
+++ ALARM.py
@@ -60,55 +60,59 @@ user()
 
 
 
-while True:
-    global re
-    global ampm,hour,min
-    cd = datetime.datetime.now()
-    chour = cd.strftime("%I")
-    chour = int(chour)
-    cmin = cd.strftime("%M")
-    cmin = int(cmin)
-    campm = cd.strftime("%p")
-    if ampm == campm:
-        if hour == chour:
-            if min == cmin:
-                music("jyotiba.mp3","stop")
+def alarm():
+    while True:
+        global re
+        global ampm,hour,min
+        cd = datetime.datetime.now()
+        chour = cd.strftime("%I")
+        chour = int(chour)
+        cmin = cd.strftime("%M")
+        cmin = int(cmin)
+        campm = cd.strftime("%p")
+        if ampm == campm:
+            if hour == chour:
+                if min == cmin:
+                    music("jyotiba.mp3","stop")
 
-                def ren():
-                    global re
-                    try:
-                        re = int(input("Enter 1 to reset alarm or 0 to exit the alarm: "))
+                    def ren():
+                        global re
+                        try:
+                            re = int(input("Enter 1 to reset alarm or 0 to exit the alarm: "))
+                            if re == 1:
+                                user()
+                                alarm()
+                            elif re == 0:
+                                def end(file):
+                                    mixer.init()
+                                    mixer.music.load(file)
+                                    mixer.music.play(-1)
+                                    print("Thank you! By Vinayak Maskar!")
 
-                    except Exception as error:
-                        print("Enter 1 or 0!")
-                        return ren()
-                ren()
+                                    while True:
+                                        time.sleep(2)
 
+                                        mixer.music.stop()
+                                        break
 
-                if re == 1:
-                    user()
+                                end("thank3.mp3")
+                            else:
+                                print("Enter 1 or 2!!")
+                                ren()
 
-                    continue
+                        except Exception as error:
+                            print("Enter 1 or 0!")
+                            return ren()
 
-                elif re == 0:
-                    def end(file):
-                        mixer.init()
-                        mixer.music.load(file)
-                        mixer.music.play(-1)
-                        print("Thank you! By Vinayak Maskar!")
-
-                        while True:
-                            time.sleep(2)
-
-                            mixer.music.stop()
-                            break
-                    end("thank3.mp3")
+                    ren()
 
                     break
 
-                else:
-                    print("Enter 1 or 2!!")
-                    ren()
+alarm()
+
+
+
+
 
 
 
